aee661375c50902c1a66d85a3daa82ce47e70c6b
emeryplyler <63931507+emeryplyler@users.noreply.github.com>
2025-09-23 13:42:29 -0700

    html and error handling

--- unit11/async/pokemon/main.js
+++ unit11/async/pokemon/main.js
@@ -14,29 +14,37 @@ let pokeapi = "https://pokeapi.co/api/v2/";
 async function threeMons()
 {
     // get every pokemon
-    const d = await fetch(pokeapi.concat("pokemon?limit=100000&offset=0"));
-    const data = await d.json(); // convert to json
-    const toGet = 3; // how many pokemon to select at once (three in this case)
-    const totalMons = data.count; // could hard code, but they may add more pokemon someday
-    // use Array.from, use length = 3, use a mapping function to run on each created item of the array
-    // mapping function will generate a random index from 0 to totalMons - 1
-    const randomNums = Array.from({ length: toGet }, () => Math.floor(Math.random() * totalMons));
-    // make an empty array for the pokemon 
-    const pokes = Array.from({ length: toGet });
-
-    for (let i = 0; i < toGet; i++)
+    try
     {
-        const index = randomNums[i];
-        pokes[i] = data.results[index]; // get that number pokemon from the fetch results
-    }
+        const d = await fetch(pokeapi.concat("pokemon?limit=100000&offset=0"));
+        const data = await d.json(); // convert to json
+        const toGet = 3; // how many pokemon to select at once (three in this case)
+        const totalMons = data.count; // could hard code, but they may add more pokemon someday
 
-    // now we have three objects, each with a name and a url
-    for (let i in pokes)
+        // use Array.from, use length = 3, use a mapping function to run on each created item of the array
+        // mapping function will generate a random index from 0 to totalMons - 1
+        const randomNums = Array.from({ length: toGet }, () => Math.floor(Math.random() * totalMons));
+        // make an empty array for the pokemon 
+        const pokes = Array.from({ length: toGet });
+
+        for (let i = 0; i < toGet; i++)
+        {
+            const index = randomNums[i];
+            pokes[i] = data.results[index]; // get that number pokemon from the fetch results
+        }
+
+        // now we have three objects, each with a name and a url
+        for (let i in pokes)
+        {
+            console.log(`Name: ${pokes[i].name}, URL: ${pokes[i].url}`);
+        }
+
+        return pokes; // return pokemon names and urls for speciesInfo() to use
+    }
+    catch (error)
     {
-        console.log(`Name: ${pokes[i].name}, URL: ${pokes[i].url}`);
+        console.error(`Couldn't retrieve Pokemon: ${error}`);
     }
-
-    return pokes; // return pokemon names and urls for speciesInfo() to use
 }
 // threeMons();
 
@@ -46,30 +54,38 @@ async function threeMons()
 // takes in a pokemon's regular url, then goes to the species url and returns info from there
 async function speciesInfo(url)
 {
-    // retrieve base data
-    const d = await fetch(url);
-    const data = await d.json();
-    const specUrl = data.species.url; // grab species url from base data
-
-    // get species data
-    const s = await fetch(specUrl);
-    const specData = await s.json();
-
-    // search specData.flavor-text-entries for english results, console log the first one found
-    for (let i = 0; i < specData.flavor_text_entries.length; i++)
+    try
     {
-        const entry = specData.flavor_text_entries[i];
-        if (entry.language.name === "en")
+        // retrieve base data
+        const d = await fetch(url);
+        const data = await d.json();
+        const specUrl = data.species.url; // grab species url from base data
+
+        // get species data
+        const s = await fetch(specUrl);
+        const specData = await s.json();
+
+        // search specData.flavor-text-entries for english results, console log the first one found
+        for (let i = 0; i < specData.flavor_text_entries.length; i++)
         {
-            console.log(entry.flavor_text);
-            return entry.flavor_text; // return entry for later use
+            const entry = specData.flavor_text_entries[i];
+            if (entry.language.name === "en")
+            {
+                console.log(entry.flavor_text);
+                return [data, entry.flavor_text]; // return data and entry for later use
+            }
+            // else, continue        
         }
-        // else, continue        
     }
+    catch (error)
+    {
+        console.error(`Couldn't retrieve Pokemon species: ${error}`);
+    }
+
 }
 
 // chooses three random pokemon and prints their names and some information to the console
-async function printThree()
+async function speciesThree()
 {
     const pokemon = await threeMons(); // get array of three pokemon
     // store species info in another array for later
@@ -78,13 +94,16 @@ async function printThree()
     {
         console.log(`${pokemon[i].name}: `); // print name
         const info = await speciesInfo(pokemon[i].url); // call species function on url
-        // speciesInfo() will print the information
-        pokemonInfo.push([pokemon[i], info]); // add the pokemon and its dex entry to new info array
+        // speciesInfo() will print the information to the console
+
+        const data = info[0]; // the first item returned was all the pokemon's data
+        const entry = info[1]; // the second item was the dex entry
+        pokemonInfo.push([data, entry]); // add the pokemon and its dex entry to new info array
     }
     return pokemonInfo; // this will be helpful for part 4
 }
 
-printThree();
+speciesThree();
 
 // 4: using html
 
@@ -100,16 +119,43 @@ document.addEventListener("DOMContentLoaded", () =>
         // add a function to the button clicking
         button.addEventListener("click", async () =>
         {
-            const pokes = await threeMons();
+            // remove previous pokemon, if there are any
+            // uses while instead of for because iterating over something while deleting things from it can cause issues
+            while (entrySpot.hasChildNodes())
+            {
+                entrySpot.removeChild(entrySpot.lastChild); // remove one of the nodes
+            }
+
+            const pokes = await speciesThree(); // get three sets of pokemon and dex entries
+            for (let mon in pokes)
+            {
+                // each item in pokes is a pair of two things: 'data', 'entry'
+                // data has all base information about the pokemon, including name and image
+                // entry is the flavor text as a string
+                const pair = pokes[mon];
+                // make a new container element that will hold all the pokemon's info in it
+                const pokeball = document.createElement("div");
+                entrySpot.appendChild(pokeball); // add to DOM
+
+                // make a name tag
+                const nametag = document.createElement("h2");
+                nametag.textContent = pair[0].name; // use the 'name' property of the first item in the pair, 'data'
+                pokeball.appendChild(nametag); // add to new container
+
+                // make a portrait
+                const sprite = document.createElement("img");
+                sprite.src = pair[0].sprites["front_default"]; // 'data' has a dictionary of sprite type names to sprite urls
+                pokeball.appendChild(sprite);
+
+                // show the flavor text
+                const para = document.createElement("p");
+                para.textContent = pair[1]; // set equal to 'entry'
+                pokeball.appendChild(para);
+            }
         });
     }
     catch (err)
     {
         console.error(err);
     }
-    // call threemons and get an array of three random pokemon names and urls
-    // for each pokemon:
-    // make a new box and add a text box to display the name in it
-    // add an image and use the front default sprite
-    // call the species info function and add that to a text box under the sprite
 });
