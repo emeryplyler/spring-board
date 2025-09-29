250d74ce826087e5d14c45a565edfabf448f2490
emeryplyler <63931507+emeryplyler@users.noreply.github.com>
2025-09-29 10:42:51 -0700

    5 finished

---
+++ unit11/11.11/modules/5/app.mjs
@@ -0,0 +1,6 @@
+import "./globalConfig.mjs"
+
+// by importing this file, all that file's code will run first
+
+// then this one will execute
+console.log("Configuration loaded");
---
+++ unit11/11.11/modules/5/globalConfig.mjs
@@ -0,0 +1,2 @@
+// will import this file into another file
+console.log("Global configuration setup started");
