bbbcc78e83b289c52fd068588275078945663a89
emeryplyler <63931507+emeryplyler@users.noreply.github.com>
2025-09-30 13:26:46 -0700

    implement insert iterative

--- unit11/11.13/dsa-bsts/binary-search-tree.js
+++ unit11/11.13/dsa-bsts/binary-search-tree.js
@@ -15,14 +15,43 @@ class BinarySearchTree {
    * Returns the tree. Uses iteration. */
 
   insert(val) {
+    // if empty tree, make this the root
+    if (!this.root) {
+      this.root = new Node(val); // make new node
+      return this; // return self
+    }
 
+    let currentNode = this.root; // start at root
+    while (true) {
+      if (currentNode.val > val) {
+        if (currentNode.left === null) { // currentNode has an open space on the left
+          currentNode.left = new Node(val); // insert here
+          break;
+        } else {
+          // currentNode already has a left node; check that left node and keep looking for an open spot
+          currentNode = currentNode.left;
+        }
+      } else if (currentNode.val < val) {
+        if (currentNode.right === null) {
+          currentNode.right = new Node(val); // insert on the right
+          break;
+        } else {
+          currentNode = currentNode.right;
+        }
+      } else {
+        // there's a small chance that currentNode.val and val will be equivalent
+        // in this situation, to keep the order of the tree, just don't insert the value
+        break;
+      }
+    }
+    return this;
   }
 
   /** insertRecursively(val): insert a new node into the BST with value val.
    * Returns the tree. Uses recursion. */
 
   insertRecursively(val) {
-
+    
   }
 
   /** find(val): search the tree for a node with value val.
