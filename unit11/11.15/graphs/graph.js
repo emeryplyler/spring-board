22c2fa598d292e760dfe699d835859eaeb24935d
emeryplyler <63931507+emeryplyler@users.noreply.github.com>
2025-10-01 12:32:31 -0700

    add basic functions

--- unit11/11.15/graphs/graph.js
+++ unit11/11.15/graphs/graph.js
@@ -11,19 +11,37 @@ class Graph {
   }
 
   // this function accepts a Node instance and adds it to the nodes property on the graph
-  addVertex(vertex) {}
+  addVertex(vertex) {
+    this.nodes.add(vertex); // try to add the vertex to the set
+    // if it's already in there, nothing will change
+  }
 
   // this function accepts an array of Node instances and adds them to the nodes property on the graph
-  addVertices(vertexArray) {}
+  addVertices(vertexArray) {
+    for (const vertex of vertexArray) {
+      this.nodes.add(vertex);
+    }
+  }
 
   // this function accepts two vertices and updates their adjacent values to include the other vertex
-  addEdge(v1, v2) {}
+  addEdge(v1, v2) {
+    v1.adjacent.add(v2);
+    v2.adjacent.add(v1);
+  }
 
   // this function accepts two vertices and updates their adjacent values to remove the other vertex
-  removeEdge(v1, v2) {}
+  removeEdge(v1, v2) {
+    v1.adjacent.delete(v2);
+    v2.adjacent.delete(v1);
+  }
 
   // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
-  removeVertex(vertex) {}
+  removeVertex(vertex) {
+    for (const neighbor of vertex.adjacent) {
+      this.removeEdge(neighbor, vertex); // remove vertex-to-delete from other vertices' adjacency lists
+    }
+    this.nodes.delete(vertex);
+  }
 
   // this function returns an array of Node values using DFS
   depthFirstSearch(start) {}
