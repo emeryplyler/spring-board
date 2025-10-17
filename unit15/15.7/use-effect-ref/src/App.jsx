084c8e9c8c6fe36c26e8230ccb6b63084d579145
emeryplyler <63931507+emeryplyler@users.noreply.github.com>
2025-10-17 15:58:05 -0700

    init Effects and Refs

---
+++ unit15/15.7/use-effect-ref/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import './App.css';
+
+function App()
+{
+    // display a deck of cards, one card at a time
+    // when page loads, create new deck from api and show a button that lets you draw a card
+    // every time you click the button show a new card until deck is empty
+    // if deck is empty and you click the button show 'no cards remaining' in an alert message
+
+    return (
+        <>
+            <div id='deck'>Hi</div>
+        </>
+    );
+}
+
+export default App;
