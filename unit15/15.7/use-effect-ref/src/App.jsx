6d6a370da33decf69bdb1d5b96717e7a3ceb197d
emeryplyler <63931507+emeryplyler@users.noreply.github.com>
2025-10-17 17:14:49 -0700

    implement drawing cards

--- unit15/15.7/use-effect-ref/src/App.jsx
+++ unit15/15.7/use-effect-ref/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App()
@@ -8,9 +8,67 @@ function App()
     // every time you click the button show a new card until deck is empty
     // if deck is empty and you click the button show 'no cards remaining' in an alert message
 
+    const [deck, updateDeck] = useState(null);
+
+    const deckElement = document.getElementById("deck");
+
+    useEffect(() =>
+    {
+        // called ONLY when the page loads, pulls from the api
+        // ajax is async and useEffect can't be async, so we define a new async function inside here
+
+        // notes: apparently, strict mode will render components twice in dev mode to see if there are issues;
+        // when run in dev mode, initializeDeck will be called twice when the page loads and never again
+        async function initializeDeck()
+        {
+            try
+            {
+                const response = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
+                let newDeck = { deckID: response.data.deck_id, remaining: response.data.remaining }; // reformat
+                updateDeck(newDeck);
+            }
+            catch (err)
+            {
+                console.error(err);
+            }
+        }
+        // now actually call the function and make the deck
+        initializeDeck();
+
+    }, []);
+
+    function onDrawButtonPress()
+    {
+        // called by the draw button
+        if (deck.remaining <= 0)
+        {
+            console.log("No cards left!");
+            return;
+        }
+
+        // call the api to draw a new card
+        // because this process is async, this function will just start the async function and return
+        drawCard();
+    }
+
+    async function drawCard()
+    {
+        const response = await axios.get(`https://deckofcardsapi.com/api/deck/${deck.deckID}/draw/?count=1`);
+        let card = response.data.cards[0]; // get first item, since there will always only be one
+        let newDeck = { ...deck, remaining: response.data.remaining }; // make a new version of deck with the new 'remaining' count
+        updateDeck(newDeck);
+
+        // display the card drawn
+        let cardElement = document.createElement("img");
+        cardElement.src = card.image;
+        cardElement.id = "card";
+        deckElement.appendChild(cardElement);
+    }
+
     return (
         <>
-            <div id='deck'>Hi</div>
+            <button onClick={onDrawButtonPress}>Draw</button>
+            <div id='deck'></div>
         </>
     );
 }
