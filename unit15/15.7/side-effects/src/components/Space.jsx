479f538a73a10628b99a9218f8c3e5f63d6855bc
emeryplyler <63931507+emeryplyler@users.noreply.github.com>
2025-10-20 16:39:20 -0700

    implement remove stars

--- unit15/15.7/side-effects/src/components/Space.jsx
+++ unit15/15.7/side-effects/src/components/Space.jsx
@@ -9,6 +9,7 @@ function Space()
     const STAR_SIZE = 5;
     const [stars, setStars] = useState([]); // array of references to star objects
     let intervalID = useRef(null);
+    let idCounter = useRef(0); // increment to make star ids
     // Implement the logic to generate stars at random positions within the viewport (Hint: x position is window.innerWidth - STAR_SIZE multiplied by a random number within 0 and 1). Each star should have a unique identifier.
     function makeStar()
     {
@@ -17,19 +18,40 @@ function Space()
         let posy = Math.floor(Math.random() * (window.innerHeight - STAR_SIZE));
 
         // make new star object
-        let newStar = { x: posx, y: posy };
+        let newStar = { x: posx, y: posy, id: idCounter.current };
+        idCounter.current++;
+
         // add it to the array
 
         // notes: based on the react docs, for updating a state variable based on its previous value, you MUST use an updater function like the arrow function below
         // an updater function has one argument, 'pending state', and returns 'next state'
         // react puts all calls to the updater function in a queue so they will still happen in the correct order
         setStars(prevStars => [...prevStars, newStar]);
-        console.log(intervalID);
     }
 
-    // makeStar();
-
     // function to destroy star by id
+    // target.id?
+    function removeStar(id)
+    {
+        // locate object in array with this id
+        try
+        {
+            let elem = stars.find(star => star.id === id);
+            let ind = stars.indexOf(elem);
+            if (ind != -1) 
+            {
+                // notes: splice doesn't work as expected on state arrays
+                // instead, make a new array including everything except what you want to remove, then set state to that
+                setStars(stars.filter(star => star.id != id));
+            }
+        }
+        catch (error)
+        {
+            console.error(error);
+            return;
+        }
+
+    }
 
     // useEffect for interval adding new star every 2.5 sec
     let going = false; // on page load, the browser keeps calling this twice so I added this boolean
@@ -37,18 +59,14 @@ function Space()
     {
         if (!going)
         {
-            console.log("starting interval");
             intervalID.current = setInterval(makeStar, 2500);
             going = true;
-            console.log(intervalID);
         }
     }, []);
 
     // clean up interval
     function endInterval()
     {
-        console.log("clearing");
-        console.log(intervalID);
         intervalID.current && clearInterval(intervalID.current);
     }
 
@@ -59,9 +77,9 @@ function Space()
             <button id="end-interval-button" onClick={endInterval}> stop making stars </button>
             <div className="space">
                 {
-                    stars.map((star, index) =>
+                    stars.map((star) =>
                     {
-                        return <Star key={index} x={star.x} y={star.y} />;
+                        return <Star key={star.id} id={star.id} x={star.x} y={star.y} destroy={removeStar} />;
                     })
                 }
             </div>
--- unit15/15.7/side-effects/src/components/Star.jsx
+++ unit15/15.7/side-effects/src/components/Star.jsx
@@ -1,9 +1,10 @@
 // represents individual stars that are randomly placed in space. These components need to be focusable and destructible upon user interaction
 
-function Star({x, y}) 
+function Star({id, x, y, destroy}) 
 {
+    // add event listener to click, destroy
     return (
-        <div className="star" style={{position: 'absolute', left: x, top: y}}> </div>
+        <div tabIndex="0" className="star" onClick={() => destroy(id)} style={{position: 'absolute', left: x, top: y}}> </div>
     );
 }
 
