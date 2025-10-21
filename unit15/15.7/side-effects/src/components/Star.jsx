eb19df21b59d8e99b10c50477ad7ee500af234ed
emeryplyler <63931507+emeryplyler@users.noreply.github.com>
2025-10-21 10:35:56 -0700

    add star focus and focus effect

--- unit15/15.7/side-effects/src/components/Star.jsx
+++ unit15/15.7/side-effects/src/components/Star.jsx
@@ -1,10 +1,22 @@
 // represents individual stars that are randomly placed in space. These components need to be focusable and destructible upon user interaction
 
-function Star({id, x, y, destroy}) 
+import { useEffect, useRef } from "react";
+
+function Star({ id, x, y, destroy }) 
 {
+    // make reference to star when star is created
+    // notes: to keep a reference to the element, initialize the reference to a placeholder value (null), then set it in the element using ref={starRef}
+    const starRef = useRef(null);
+
+    // focus on star when star is created (empty dependency list)
+    useEffect(() =>
+    {
+        starRef.current.focus();
+    }, []);
+
     // add event listener to click, destroy
     return (
-        <div tabIndex="0" className="star" onClick={() => destroy(id)} style={{position: 'absolute', left: x, top: y}}> </div>
+        <div ref={starRef} tabIndex="0" className="star" onClick={() => destroy(id)} style={{ position: 'absolute', left: x, top: y }}> </div>
     );
 }
 
