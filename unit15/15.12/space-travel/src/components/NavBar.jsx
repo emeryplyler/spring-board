cc4161c2b9d8ab89d8497a35b8229e1fe4618b22
emeryplyler <63931507+emeryplyler@users.noreply.github.com>
2025-10-31 12:28:18 -0700

    add navbar styles

--- unit15/15.12/space-travel/src/components/NavBar.jsx
+++ unit15/15.12/space-travel/src/components/NavBar.jsx
@@ -7,9 +7,9 @@ export default function NavBar({ routes })
 {
     return (
         <nav>
-            <NavLink key={0} to={`/`}>Home</NavLink>
+            <NavLink className="nav-link" key={0} to={`/`}>Home</NavLink>
             {routes.map((route, index) => (
-                <NavLink key={index + 1} to={`/${route.toLowerCase()}`}>{route}</NavLink>
+                <NavLink className="nav-link" key={index + 1} to={`/${route.toLowerCase()}`}>{route}</NavLink>
             ))}
         </nav>
     );
