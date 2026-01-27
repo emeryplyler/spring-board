2894636658e5568b5d1a92cbc59dcc8f54cd8a27
Emery <63931507+emeryplyler@users.noreply.github.com>
2026-01-26 16:21:34 -0800

    add temp routes

--- unit19/19.5/express-mongo-notes/app.js
+++ unit19/19.5/express-mongo-notes/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Blog = require('./models/blog'); // import blog document model
 
 app = express();
 
@@ -10,6 +11,51 @@ mongoose.connect(mongoURI)
 
 app.get("/", (req, res) => {
     res.status(200).send("<p>Homepage</p>");
+});
+
+app.get("/add-blog", (req, res) => {
+    // make a new instance of Blog document
+    const newBlog = new Blog({
+        // add properties; mongo will handle the timestamps
+        title: "New Blog!",
+        snippet: "about my new blog",
+        body: "more about my new blog blah blah blah blah"
+    });
+
+    // call the save() method of the model
+    // this will save it to the collection (async)
+    newBlog.save()
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    // it worked!
+})
+
+app.get("/all-blogs", (req, res) => {
+    // use blog model to get all documents in the collection
+    Blog.find() // since Blog is attached to the 'blogs' collection, it will find all blogs
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    // it worked
+});
+
+app.get("/single-blog", (req, res) => {
+    // mongo will handle converting the string into the objectId
+    Blog.findById('6978030a51b49b18095f8751')
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    // it worked
 })
 
 app.listen(3000);
--- unit19/19.5/express-mongo-notes/models/blog.js
+++ unit19/19.5/express-mongo-notes/models/blog.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// make a new schema, then create a model using that schema
+
+// creating a new schema:
 // define properties that blog documents should have
 const blogSchema = new Schema({
     title: {
@@ -16,3 +19,11 @@ const blogSchema = new Schema({
         required: true
     }
 }, { timestamps: true });
+
+// creating a new model using a schema:
+// the name is important; when you name it 'Blog', it will look for a collection called 'blogs' automatically
+// this is basically assigning this model to an existing collection
+const Blog = mongoose.model('Blog', blogSchema);
+
+// export the model:
+module.exports = Blog;
