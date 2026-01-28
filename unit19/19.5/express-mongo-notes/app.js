c354c1cc8adb49a4374ddb851cfdc7d7cca4e345
Emery <63931507+emeryplyler@users.noreply.github.com>
2026-01-28 11:36:33 -0800

    clean up

--- unit19/19.5/express-mongo-notes/app.js
+++ unit19/19.5/express-mongo-notes/app.js
@@ -19,46 +19,17 @@ app.use(express.urlencoded({ extended: true })); // passes url encoded data as a
 app.get("/", (req, res) => {
     // use ejs to render a view
     // express will look for 'index' in the views folder
-    const blogs = [
-        { title: "nice", snippet: "post" },
-        { title: "good", snippet: "job on that one" }
-    ];
-    // render index.ejs and pass in an object holding data
-    // because the property name and value name are the same, you can just write 'blogs'
-    res.render('index', { title: "Home", blogs }); // second parameter is a data object that will be sent to the ejs file
-});
 
-app.get("/add-blank-blog", (req, res) => {
-    // make a new instance of Blog document
-    const newBlog = new Blog({
-        // add properties; mongo will handle the timestamps
-        title: "New Blog!",
-        snippet: "about my new blog",
-        body: "more about my new blog blah blah blah blah"
-    });
-
-    // call the save() method of the model
-    // this will save it to the collection (async)
-    newBlog.save()
-        .then((result) => {
-            res.send(result);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-    // it worked!
-})
-
-app.get("/blogs", (req, res) => {
-    // use blog model to get all documents in the collection
     Blog.find() // since Blog is attached to the 'blogs' collection, it will find all blogs
         .then((result) => {
-            res.send(result);
+            // instead of res.send, render a view instead
+            // render index.ejs and pass in an object holding data
+            // because the property name and value name are the same, you can just write 'blogs'
+            res.render('index', { title: "Blogs", blogs: result }); // second parameter is the data object that will be sent to the ejs file
         })
         .catch((err) => {
             console.log(err);
         });
-    // it worked
 });
 
 // show form to make new blog
@@ -67,17 +38,22 @@ app.get("/blogs/create", (req, res) => {
 });
 
 // handle post requests
+// note: the url here is /blogs and not /blogs/create because in the create.ejs file we specify that action=/blogs
 app.post("/blogs", (req, res) => {
+    // make a new instance of Blog document
     const newBlog = new Blog(req.body); // can just pass object directly into constructor
+    // call the save() method of the model
+    // this will save it to the collection (async)
     newBlog.save()
         .then((result) => {
-            res.redirect("/blogs"); // send back to blogs page
+            res.redirect("/"); // send back to blogs page
         })
         .catch((err) => {
             console.log(err);
         });
 });
 
+// handle get requests for a specific blog
 app.get("/blogs/:blogid", (req, res) => {
     const id = req.params.blogid;
     Blog.findById(id)
