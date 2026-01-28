fd74c0ed1fd4d8cd0f131aae414c0d31f2a99918
Emery <63931507+emeryplyler@users.noreply.github.com>
2026-01-28 14:06:43 -0800

    change -move routes into new file

--- unit19/19.5/express-mongo-notes/app.js
+++ unit19/19.5/express-mongo-notes/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const mongoose = require('mongoose');
-const Blog = require('./models/blog'); // import blog document model
+const blogRoutes = require('./routes/blogRoutes'); // import router for all blog-related pages
 
 app = express();
 
@@ -19,66 +19,6 @@ app.use(express.urlencoded({ extended: true })); // passes url encoded data as a
 // static files
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    // use ejs to render a view
-    // express will look for 'index' in the views folder
-
-    Blog.find() // since Blog is attached to the 'blogs' collection, it will find all blogs
-        .then((result) => {
-            // instead of res.send, render a view instead
-            // render index.ejs and pass in an object holding data
-            // because the property name and value name are the same, you can just write 'blogs'
-            res.render('index', { title: "Blogs", blogs: result }); // second parameter is the data object that will be sent to the ejs file
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
-
-// show form to make new blog
-app.get("/blogs/create", (req, res) => {
-    res.render("create", { title: "New Blog" });
-});
-
-// handle post requests
-// note: the url here is /blogs and not /blogs/create because in the create.ejs file we specify that action=/blogs
-app.post("/blogs", (req, res) => {
-    // make a new instance of Blog document
-    const newBlog = new Blog(req.body); // can just pass object directly into constructor
-    // call the save() method of the model
-    // this will save it to the collection (async)
-    newBlog.save()
-        .then((result) => {
-            res.redirect("/"); // send back to blogs page
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
-
-// handle get requests for a specific blog
-app.get("/blogs/:blogid", (req, res) => {
-    const id = req.params.blogid;
-    Blog.findById(id)
-        .then((result) => {
-            res.render("details", { blog: result, title: "Blog Details" });
-        })
-        .catch((err) => {
-            console.log(err)
-        });
-});
-
-// handle delete requests for a specific blog
-app.delete("/blogs/:blogid", (req, res) => {
-    const id = req.params.blogid;
-    Blog.findByIdAndDelete(id)
-        .then(result => {
-            // when doing ajax requests you can't redirect the same way
-            res.json({ redirect: '/' }); // we pass a json with a redirect property
-        })
-        .catch(err => {
-            console.log(err);
-        });
-});
+app.use("/blogs", blogRoutes); // activate the blogRoutes file
 
 app.listen(3000);
