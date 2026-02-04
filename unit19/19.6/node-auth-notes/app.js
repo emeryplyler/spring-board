ac93e5126979f9656dfd46cdd8da9b20da08183e
Emery <63931507+emeryplyler@users.noreply.github.com>
2026-02-04 11:20:21 -0800

    add cookies, login, signup pages

--- unit19/19.6/node-auth-notes/app.js
+++ unit19/19.6/node-auth-notes/app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config({quiet: true});
 const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -21,3 +23,9 @@ mongoose.connect(process.env.MONGO_URI)
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes); // add in all the routes from authRoutes.js
+
+// cookies
+app.get('/set-cookies', (req, res) => {
+  res.setHeader('Set-Cookie', 'newUser=true');
+  res.send('got the cookie');
+})
