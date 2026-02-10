861d076887604aefede3846d65677c7a76ca5e0b
Emery <63931507+emeryplyler@users.noreply.github.com>
2026-02-09 16:57:34 -0800

    add get and specific get

--- unit19/19.8/mongodb-express-notes/app.js
+++ unit19/19.8/mongodb-express-notes/app.js
@@ -1,15 +1,60 @@
 const express = require("express");
+const { ObjectId } = require("mongodb");
+const { connectToDb, getDb } = require("./db");
 
 // init app
 const app = express();
-app.listen(3000, () => {
-    console.log("App listening on port 3000");
+
+// connect to db
+let db;
+connectToDb((err) => {
+    // function to be called right after connection attempt
+    // err is only passed if there was an error
+    if (!err) {
+        // success; start listening
+        app.listen(3000, () => {
+            console.log("App listening on port 3000");
+        });
+        db = getDb(); // returns database connection, which was retrieved on calling connectToDb
+    }
 });
+
 
 // middleware
 // middleware goes here
 
 // routes
+
+// get many
 app.get("/books", (req, res) => {
-    res.json({ message: "Welcome to the API" });
+    let books = [];
+
+    db.collection("books")
+        .find() // returns a cursor pointing to a set of documents; with no arguments, points to entire collection
+        // note; mongodb fetches documents in little batches of 100 or so
+        .sort({ author: 1 }) // sort by author name
+        .forEach(book => books.push(book)) // add all books in batch to the array
+        .then(() => {
+            res.status(200).json(books);
+        })
+        .catch(() => { res.status(500).json({ error: "couldn't fetch documents" }); });
+});
+
+// get one
+app.get("/books/:id", (req, res) => {
+    // validate id format
+    if (ObjectId.isValid(req.params.id)) {
+        // valid id; check collection for corresponding book
+        db.collection("books")
+            .findOne({ _id: ObjectId(req.params.id) }) // convert route params into ObjectId, filter by _id
+            .then(doc => {
+                res.status(200).json(doc); // send in the doc object returned by findOne()
+            })
+            .catch(err => {
+                res.status(500).json({ message: "Couldn't fetch that document" });
+            });
+    } else {
+        // object id was not valid; don't bother checking collection
+        res.status(500).json({ error: "Not a valid document ID" });
+    }
 });
--- unit19/19.8/mongodb-express-notes/db.js
+++ unit19/19.8/mongodb-express-notes/db.js
@@ -4,14 +4,16 @@ let dbConnection; // initialize db connection variable in broad scope
 
 // connecting to db
 module.exports = {
-    connectToDb: () => {
+    connectToDb: (callback) => {
         // connection string
         MongoClient.connect("mongodb://localhost:27017/bookstore")
             .then((client) => {
                 dbConnection = client.db(); // set db connection
+                return callback(); // the function that is called right after the connection attempt
             })
             .catch(err => {
                 console.log(err);
+                return callback(err); // pass the error to the callback function for error handling
             });
     },
     getDb: () => dbConnection // return db connection (implicit return)
